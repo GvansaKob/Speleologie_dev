e03ecd6a1a6050f562660d13952d1b0c58f0bf47
Gvansa Kobesashvili <gvansa.kobesashvili@my-digital-school.org>
2024-04-19 12:17:44 +0200

    CSS header

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <section class="header">
+    <section id="header">
         <img class="logo" src="https://mathieutu.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F012141a6-a777-45c9-95f5-b566bcc8971b%2F3e3a4294-a46f-4c48-a7e9-4b0d116eb362%2Flogo-ffs.webp?table=block&id=00844314-a6ab-4257-94e9-dbac68aae6f3&spaceId=012141a6-a777-45c9-95f5-b566bcc8971b&width=250&userId=&cache=v2" alt="">
         <nav>
             <a href="{{route('experiences')}}">Expériences</a>
--- resources/views/experiences.blade.php
+++ resources/views/experiences.blade.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <section class="header">
+    <section id="header">
         <img class="logo" src="https://mathieutu.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F012141a6-a777-45c9-95f5-b566bcc8971b%2F3e3a4294-a46f-4c48-a7e9-4b0d116eb362%2Flogo-ffs.webp?table=block&id=00844314-a6ab-4257-94e9-dbac68aae6f3&spaceId=012141a6-a777-45c9-95f5-b566bcc8971b&width=250&userId=&cache=v2" alt="">
         <nav>
             <a href="{{route('experiences')}}">Expériences</a>
@@ -18,7 +18,7 @@
         </nav>
     </section>
 
-    <form method="POST" action="{{route('experiences')}}">
+    <form id="xperiences" method="POST" action="{{route('experiences')}}">
         @csrf
         <p>
             <label for="email">E-mail</label><br>
@@ -47,20 +47,20 @@
         </p>
         <label for="select">Êtes-vous satisfait de votre expérience ? :</label><br>
         <select id="select" name="select">
-            <option value="Peu satisfait">Peu satisfait</option>
-            <option value="Pas satisfait">Pas satisfait</option>
+            <option value="Pas satisfait">Peu satisfait</option>
+            <option value="Peu satisfait">Pas satisfait</option>
             <option value="Moyennement satisfait">Moyennement satisfait</option>
             <option value="Satisfait">Satisfait</option>
             <option value="Très satisfait">Très satisfait</option>
-        </select><br><br>
-
+        </select><br>
+        <p>Acceptez-vous d'être contacté par email ?</p>
         <input type="radio" name="reception_email" value="oui" checked>
         Oui
         </label> <br>
         <label>
             <input type="radio" name="reception_email" value="non">
             Non
-        </label><br>
+        </label><br><br>
 
         <a href="{{route('experiences')}}">
             <button type="submit">Soumettre</button>
